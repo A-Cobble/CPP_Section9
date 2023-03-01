66b532e6a5a0ae92dcda14e8fd4ed3b9d9a35104
A-Cobble <alex.cobble42@gmail.com>
2023-02-28 17:38:45 -0800

    Practicing for loops in C++

---
+++ forLoop/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+	/*
+		for loops are made up of 3 expressions
+			for (initialization ; condition ; increment) {
+				statements(s);
+			}
+	*/
+
+	/*
+		for (int i{ 0 }; i < 5; i++) {
+			cout << i << endl;
+		}
+	*/
+
+	/*
+		for (int i{ 1 }; i <= 10; i++) {
+			cout << i << endl;
+		}
+
+		for (int i{ 1 }; i <= 10; i += 2) {
+			cout << i << endl;
+		}
+
+		for (int i{ 10 }; i >= 0; i--) {
+			cout << i << endl;
+		}
+		cout << "Blastoff!" << endl;
+	*/
+
+	/*
+		for (int i{ 10 }; i <= 100; i += 10) {
+			if (i % 15 == 0) {
+				cout << i << endl;
+			}
+		}
+	*/
+
+	for (int j{ 1 }, k{ 5 }; j <= 5; j++, k++) {
+		cout << j << " * " << k << " : " << (j * k) << endl;
+		cout << j << " + " << k << " = " << (j + k) << endl;
+	}
+
+	cout << endl;
+	return 0;
+}
